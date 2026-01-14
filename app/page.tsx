52f9bf7eb5ec75a3be5ad24e35c2fac1fc18b6d4
Vinculacionutc <cdf009q@gmail.com>
2026-01-14 07:00:54 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motisdsaon';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Testimonials from '@/components/testimonials';
 import EmpresaList from '@/components/companies';
